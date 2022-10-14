d2b87de30cbd05f19b67b2df0d9495e590494c71
fedpre <fed.pregnolato@gmail.com>
2022-10-14 12:11:34 +0200

    Nextra boiler plate working

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
-import 'nextra-theme-docs/style.css';
+import 'nextra-theme-blog/style.css'
+import Head from 'next/head'
 
-export default function Nextra ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/main.css'
+
+export default function Nextra({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+        {/* <link
+          rel="preload"
+          href="/fonts/Inter-roman.latin.var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-  
